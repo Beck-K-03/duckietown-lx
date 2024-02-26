82486d725dcd11afd265a3bd4595218124dbb79d
Beck-K-03 <beckkehoe@gmail.com>
2024-02-26 18:48:49 -0500

    possiblealgo

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -1,14 +1,12 @@
 from typing import Tuple
-
+from braitenberg_agent.agent import preprocess
 import numpy as np
 
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    res[:, :shape[1] // 2] = preprocess[:, :shape[1] // 2] / 255
     # ---
     return res
 
@@ -16,7 +14,6 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:300] = -1
+    res[:, shape[1] // 2:] = preprocess[:, shape[1] // 2:] / 255
     # ---
     return res
