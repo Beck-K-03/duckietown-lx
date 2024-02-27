02603ad7d27cc95ee056b328a3807fc8326be892
Beck-K-03 <beckkehoe@gmail.com>
2024-02-26 22:48:48 -0500

    Final Changes

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -1,28 +1,47 @@
 from typing import Tuple
-#from braitenberg_agent.agent import preprocess
 import numpy as np
-
+import math
+#Decay is decay rate
+#yrange is the highest value that the exponent reaches
+#Radius is the radius of the cirlce used to make the matrix
+Decay=0.99056
+YRange=355
+Radius=300
+#the double for loop generates the circle while the single at the bottom generates the exponential decay
 
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[300:, 0:200] = 1
-    res[300:, 400:600] = 0
-    res[0:299, 201:399]=1
-    #res[:, :shape[1] // 2] = preprocess[:, :shape[1] // 2] / 255
-    # ---
+
+    for k in range (Radius):
+        for i in range(640):
+            num=(k**2)-((i-320)**2)
+            if (num>=0):
+                highpixel=-math.sqrt(num)+480
+                highpixel=int(highpixel)
+                if (i<320):
+                    res[highpixel:480,i] += (1/Radius)*((1.001)**k)
+                else:
+                    res[highpixel:480,i] += (-1/Radius)*((1.001)**k)
+    for i in range(YRange):
+        res[(479-i),320:640] += -1*(Decay**i)
+        res[(479-i),0:320] += 1*(Decay**i)
+
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[300:, 400:600] = 1
-    res[300:, 0:200] = 0
-    res[0:299, 201:399]=1
-
-    #res[:, shape[1] // 2:] = preprocess[:, shape[1] // 2:] / 255
-    # ---
+    for k in range (Radius):
+        for i in range(640):
+            num=(k**2)-((i-320)**2)
+            if (num>=0):
+                highpixel=-math.sqrt(num)+480
+                highpixel=int(highpixel)
+                if (i<320):
+                    res[highpixel:480,i] += (-1/Radius)*((1.001)**k)
+                else:
+                    res[highpixel:480,i] += (1/Radius)*((1.001)**k)
+    for i in range(YRange):
+        res[(479-i),320:640] += 1*(Decay**i)
+        res[(479-i),0:320] += -1*(Decay**i)
     return res
