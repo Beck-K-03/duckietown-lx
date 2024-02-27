6cde2a3fbc799c82c50b42852c44b0043687ccf9
Beck-K-03 <beckkehoe@gmail.com>
2024-02-26 19:43:41 -0500

    possible

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -6,9 +6,13 @@ import numpy as np
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[300:, 0:200] = 1
-    res[300:, 400:600] = -1
+    if (res[300:, 0:200]):
+        res=1
+    elif (res[300:, 400:600]):
+        res=0
+    else:
+        res=1
+
     #res[:, :shape[1] // 2] = preprocess[:, :shape[1] // 2] / 255
     # ---
     return res
@@ -17,10 +21,12 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[300:, 400:600] = 1
-    res[300:, 0:200] = -1
-
+    if (res[300:, 400:600]):
+        res=1
+    elif (res[300:, 0:200]):
+        res=0
+    else:
+        res=1
     #res[:, shape[1] // 2:] = preprocess[:, shape[1] // 2:] / 255
     # ---
     return res
